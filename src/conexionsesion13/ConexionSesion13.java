15394e844e9cad34617c93636b6b5ee5bf4994fa
Anghy Velasquez Rojas <nicolini15.15@gmail.com>
2023-11-11 12:39:45 -0500

    Agregacion Anghy

--- src/conexionsesion13/ConexionSesion13.java
+++ src/conexionsesion13/ConexionSesion13.java
@@ -18,6 +18,7 @@ public class ConexionSesion13 {
         System.out.println("Estoy en git");
         System.out.println("Yo tambien estoy en git :D");
         System.out.println("Asenjo está presente :)");
+        System.out.println("Anghy Velasquez esta presente :)");
     }
     
 }
