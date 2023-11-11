ac3cb40917e9688c825feb4b6bf4d0a5eff46f3a
JTinoco09 <jaime7936824@gmail.com>
2023-11-11 12:50:19 -0500

    Mi aporte Tinoco

--- src/conexionsesion13/ConexionSesion13.java
+++ src/conexionsesion13/ConexionSesion13.java
@@ -18,6 +18,8 @@ public class ConexionSesion13 {
         System.out.println("Estoy en git");
         System.out.println("Yo tambien estoy en git :D");
         System.out.println("Asenjo está presente :)");
+        System.out.println("Tinoco está presente :)");
+
     }
     
 }
